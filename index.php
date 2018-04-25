74b56ac2358e749dea5d5e43f80accc74bf7648c
刘博平(焦点) <bopingliu@sohu-inc.com>
2018-04-25 18:20:43 +0800

    add dns.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PRC NET TOOL</title>
+</head>
+<body>
+    <h1 id="anetworktoolforpeopleinprc">a network tool for people in PRC</h1>
+
+    <hr />
+
+    <h2 id="resource">Resource</h2>
+
+    <h3 id="dnsoverhttps">dns-over-https</h3>
+
+    <p>file <code>/dns.php</code></p>
+
+    <p>just <a href="https://dns.google.com/">Google DNS</a> Mirror </p>
+
+    <p><strong>Demo</strong> <br />
+        <code>https://prudent-travels.000webhostapp.com/dns.php?name=img.alicdn.com&amp;edns_client_subnet=123.125.18.108</code></p>
+
+    <hr />
+</body>
+</html>
