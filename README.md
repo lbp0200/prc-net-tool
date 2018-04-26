5953351b9ce1feecd776a95126fc0a7fabfdf62e
刘博平(焦点) <bopingliu@sohu-inc.com>
2018-04-26 10:09:14 +0800

    add pac.php

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ just [Google DNS](https://dns.google.com/) Mirror
 `https://prudent-travels.000webhostapp.com/dns.php?name=img.alicdn.com&edns_client_subnet=123.125.18.108`
 
 ---
+
+### Pac File
+file `/pac.php`
+
+Build by [mono_pac](https://github.com/blackgear/mono_pac) 
+
+use proxy to foreign site and Direct to PRC site.
+
+**Demo**
+
+`https://prudent-travels.000webhostapp.com/pac.php?a=SOCKS5&b=192.168.1.1:1080`
+generate a pac file which use `SOCKS5 192.168.1.1` proxy, copy and parse to [SwitchyOmega](https://github.com/FelisCatus/SwitchyOmega) 
+
+---
--- index.php
+++ index.php
@@ -1,25 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>PRC NET TOOL</title>
-</head>
-<body>
-    <h1 id="anetworktoolforpeopleinprc">a network tool for people in PRC</h1>
-
-    <hr />
-
-    <h2 id="resource">Resource</h2>
-
-    <h3 id="dnsoverhttps">dns-over-https</h3>
-
-    <p>file <code>/dns.php</code></p>
-
-    <p>just <a href="https://dns.google.com/">Google DNS</a> Mirror </p>
-
-    <p><strong>Demo</strong> <br />
-        <code>https://prudent-travels.000webhostapp.com/dns.php?name=img.alicdn.com&amp;edns_client_subnet=123.125.18.108</code></p>
-
-    <hr />
-</body>
-</html>
+<h1 id="a-network-tool-for-people-in-prc">a network tool for people in PRC</h1>
+<hr />
+<h2 id="resource">Resource</h2>
+<h3 id="dns-over-https">dns-over-https</h3>
+<p>file <code>/dns.php</code></p>
+<p>just <a href="https://dns.google.com/">Google DNS</a> Mirror</p>
+<p><strong>Demo</strong><br />
+    <code>https://prudent-travels.000webhostapp.com/dns.php?name=img.alicdn.com&amp;edns_client_subnet=123.125.18.108</code></p>
+<hr />
+<h3 id="pac-file">Pac File</h3>
+<p>file <code>/pac.php</code></p>
+<p>Build by <a href="https://github.com/blackgear/mono_pac">mono_pac</a></p>
+<p>use proxy to foreign site and Direct to PRC site.</p>
+<p><strong>Demo</strong></p>
+<p><code>https://prudent-travels.000webhostapp.com/pac.php?a=SOCKS5&amp;b=192.168.1.1:1080</code> generate a pac file which use <code>SOCKS5 192.168.1.1</code> proxy, copy and parse to <a href="https://github.com/FelisCatus/SwitchyOmega">SwitchyOmega</a></p>
+<hr />
