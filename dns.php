41212fc6a173392584bd959d9be29fe816c3a477
刘博平(焦点) <bopingliu@sohu-inc.com>
2018-05-03 13:56:09 +0800

    add pac.php

--- dns.php
+++ dns.php
@@ -1,13 +1,18 @@
 <?php
+$is_https = false;
+if (isset($_SERVER['HTTPS'])) {
+    $is_https = true;
+}
 if (count($_GET) === 0) {
     exit();
 }
 $root = 'https://dns.google.com/resolve?';
 $parameters = [];
 foreach ($_GET as $k => $v) {
-    $dv = base64_decode(urldecode($v));
+    $dv = $is_https ? $v : base64_decode(urldecode($v));
     $parameters[] = "{$k}={$dv}";
 }
 $url = $root . (implode('&', $parameters));
 
-echo base64_encode(file_get_contents($url));
+$resp = file_get_contents($url);
+echo $is_https ? $resp : base64_encode($resp);
