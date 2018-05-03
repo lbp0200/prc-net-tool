c7e64f66a1311c88e9d4fc0ed2ae094e0347fbff
刘博平(焦点) <bopingliu@sohu-inc.com>
2018-05-03 10:22:42 +0800

    add pac.php

--- dns.php
+++ dns.php
@@ -5,8 +5,9 @@ if (count($_GET) === 0) {
 $root = 'https://dns.google.com/resolve?';
 $parameters = [];
 foreach ($_GET as $k => $v) {
-    $parameters[] = "{$k}={$v}";
+    $dv = base64_decode(urldecode($v));
+    $parameters[] = "{$k}={$dv}";
 }
 $url = $root . (implode('&', $parameters));
 
-echo file_get_contents($url);
+echo base64_encode(file_get_contents($url));
